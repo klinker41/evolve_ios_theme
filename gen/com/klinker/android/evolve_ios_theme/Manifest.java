4550b0efec4aaa9cc80cc3d4e949a9b40c3770b2
Jake Klinker <jklinker1@gmail.com>
2014-02-18 00:04:05 -0600

    added background and animations to theme for next evolve update

--- gen/com/klinker/android/evolve_ios_theme/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.klinker.android.evolve_ios_theme;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
